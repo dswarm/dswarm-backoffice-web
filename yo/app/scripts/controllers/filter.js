e84e6496d527296f5725a9041dd5a1455254b05b
Christof Rodejohann <Christof.Rodejohann@slub-dresden.de>
2013-09-12 12:33:39 +0200

    DD-11 Extend Filter to represent a live view of the filters in data component

--- yo/app/scripts/controllers/filter.js
+++ yo/app/scripts/controllers/filter.js
@@ -1,29 +1,81 @@
 'use strict';
 
 angular.module('dmpApp')
-    .controller('FilterCtrl', ['$scope','$http', 'schemaParser', 'PubSub', function ($scope, $http, schemaParser, PubSub) {
+    .controller('FilterCtrl', ['$scope','$http', '$q', 'schemaParser', 'PubSub', function ($scope, $http, $q, schemaParser, PubSub) {
 
         $scope.internalName = 'Filter Widget';
 
         $scope.component = null;
-
         $scope.filterShouldBeOpen = false;
-
         $scope.result = {};
 
-        $scope.update = function() {
+        $scope.sourceDatas = [];
 
-            angular.forEach($scope.component.filters, function(filter, key){
-                filter.name = schemaParser.getData(filter.filter).join(',');
-            });
-
-            return true;
-        }
+        var schemaPromise = $http.get('/data/schema.json')
+            , dataPromise = $http.get('/data/record.json')
+            , allPromise = $q.all([schemaPromise, dataPromise]);
 
         $http.get('/data/schema.json')
             .success(function (result) {
                 $scope.result = result;
             });
+
+        allPromise.then(function (result) {
+            var schemaResult = result[0]['data']
+                , dataResult = result[1]['data'];
+
+            $scope.result = result[0]['data'];
+
+            $scope.sourceDatas.push(
+                schemaParser.parseAny(
+                    dataResult[schemaResult['title']], schemaResult['title'], schemaResult)
+            );
+
+        });
+
+        $scope.update = function() {
+
+            var inputfilterCollection = [];
+
+            angular.forEach($scope.component.filters, function(filter){
+
+                filter.inputfilters = schemaParser.getData(filter.filter,'');
+
+                inputfilterCollection = inputfilterCollection.concat(filter.inputfilters);
+
+                if(filter.inputfilters) {
+                    filter.name = "";
+                }
+
+                var countInputfilter = 0;
+                angular.forEach(filter.inputfilters, function(inputfilter) {
+                    if(filter.name.length > 0) {
+                        filter.name += ", ";
+                    }
+                    filter.name += inputfilter.title ;
+
+                    countInputfilter++;
+                });
+                if(countInputfilter == 0) { filter.name = "new filter" };
+
+            });
+
+            angular.forEach($scope.sourceDatas, function(sourceData){
+
+                if(inputfilterCollection && inputfilterCollection[0] !== undefined && inputfilterCollection.length > 0) {
+
+                    sourceData.isFiltered = true;
+                    sourceData = schemaParser.filterData(sourceData, inputfilterCollection);
+
+                } else {
+                    sourceData.isFiltered = false;
+                    sourceData.filterNoMatch = false;
+                }
+
+            });
+
+            return true;
+        }
 
         $scope.opts = {
             backdropFade: true,
@@ -38,6 +90,7 @@ angular.module('dmpApp')
 
             $scope.component.filters.push({
                 filter : schemaParser.mapData($scope.result['title'], $scope.result, true),
+                inputfilters : [],
                 name : 'new filter'
             });
 
