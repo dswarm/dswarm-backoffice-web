1c29fc3f847de2c1873174ea3a0ecc13eb0aab87
Paul Horn <phorn@avantgarde-labs.de>
2013-09-13 16:10:38 +0200

    simple (also, faulty) update lock

--- webhook/server.py
+++ webhook/server.py
@@ -1,26 +1,62 @@
+from collections import deque
 import multiprocessing
 import subprocess
 
 from flask import Flask
 app = Flask(__name__)
 
+UPDATES_WEB = False
+updates_web = deque()
+
+UPDATES_API = False
+updates_api = deque()
+
+
+def update_web():
+    if UPDATES_WEB:
+        updates_web.appendleft(True)
+
+    else:
+
+        try:
+            t = updates_web.pop()
+            if t is True:
+                multiprocessing.Process(
+                    target=subprocess.call,
+                    args=(["make", "upgrade", "dist"],),
+                    kwargs={"cwd": "/home/dmp/dmp-backoffice-web"}).start()
+
+        except IndexError:
+            pass
+
+
+def update_api():
+    if UPDATES_API:
+        updates_api.appendleft(True)
+
+    else:
+
+        try:
+            t = updates_api.pop()
+            if t is True:
+                multiprocessing.Process(
+                    target=subprocess.call,
+                    args=(["bash", "restart.sh"],),
+                    kwargs={"cwd": "/home/dmp/datamanagement-platform"}).start()
+
+        except IndexError:
+            pass
+
 
 @app.route("/unstable", methods=['POST'])
 def trigger():
-    p1 = multiprocessing.Process(
-        target=subprocess.call,
-        args=(["make", "upgrade", "dist"],),
-        kwargs={"cwd": "/home/dmp/dmp-backoffice-web"})
+    updates_web.appendleft(True)
+    update_web()
 
-    p2 = multiprocessing.Process(
-        target=subprocess.call,
-        args=(["bash", "restart.sh"],),
-        kwargs={"cwd": "/home/dmp/datamanagement-platform"})
+    updates_api.appendleft(True)
+    update_api()
 
-    p1.start()
-    p2.start()
-
-    return "Upgrade running"
+    return "Upgrade running\n"
 
 
 if __name__ == "__main__":
