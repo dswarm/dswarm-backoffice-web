ba4db6c15bd1e188448a9a5ea9d8db90d070cf1d
Paul Horn <phorn@avantgarde-labs.de>
2013-09-16 15:40:29 +0200

    DD-64
    list all uploaded files

---
+++ yo/app/scripts/resources/file.js
@@ -0,0 +1,7 @@
+angular.module('dmpApp')
+ .factory('FileResource', ['$resource', '$window', function($resource, $window) {
+    var baseUrl = $window['dmp']['jsRoutes']['api']
+      , endpoint = 'resources/:id';
+
+    return $resource(baseUrl + endpoint);
+  }]);
