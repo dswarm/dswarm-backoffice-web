fa1ef9ccf6ad431fc4d6d696c9953a4db6a4ca88
Paul Horn <phorn@avantgarde-labs.de>
2013-09-16 15:41:41 +0200

    DD-63 upload files view

---
+++ yo/app/scripts/directives/dmp-file-upload.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('dmpApp')
+  .directive('dmpFileUpload', function () {
+    return function(scope, iElement) {
+      iElement.on('change', function(event) {
+        var file = event.target.files[0];
+        scope.$apply(function() {
+          scope.data.file = file;
+        })
+      })
+    };
+  });
