f9b8be8cf637ab2fc2820be3adc0919cb74a487b
Christof Rodejohann <Christof.Rodejohann@slub-dresden.de>
2013-09-16 14:09:19 +0200

    DD-81 Show current state in navbar

--- yo/app/scripts/app.js
+++ yo/app/scripts/app.js
@@ -3,6 +3,17 @@
 var app = angular.module('dmpApp', ['ui.state', 'ui.utils', 'ui.bootstrap.tabs', 'ui.bootstrap.modal', 'ui.compat']).
     config(['$routeProvider', function($routeProvider) {
         $routeProvider.
-            when('/', {templateUrl: 'views/perspectives/data.html',   controller: DataCtrl}).
-            when('/model', {templateUrl: 'views/perspectives/model.html', controller: ModelCtrl});
+            when('/', {
+                templateUrl: 'views/perspectives/data.html',
+                controller: DataCtrl
+            }).
+            when('/model', {
+                templateUrl: 'views/perspectives/model.html',
+                controller: ModelCtrl
+            });
+    }]).run(['$rootScope', '$location', function($rootScope, $location){
+        var path = function() { return $location.path();};
+        $rootScope.$watch(path, function(newVal){
+            $rootScope.activetab = newVal;
+        });
     }]);
