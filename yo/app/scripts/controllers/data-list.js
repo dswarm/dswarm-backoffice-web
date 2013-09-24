b97ddebed737a1df7bea81324540d43a9a7b92ee
Christof Rodejohann <Christof.Rodejohann@slub-dresden.de>
2013-09-24 15:20:54 +0200

    DD-114 create table for data sets

--- yo/app/scripts/controllers/data-list.js
+++ yo/app/scripts/controllers/data-list.js
@@ -4,4 +4,19 @@ angular.module('dmpApp')
     .controller('DataListCtrl', ['$scope', '$routeParams', 'FileResource', function ($scope, $routeParams, FileResource) {
 
         $scope.files = FileResource.query();
+        $scope.selectedSet = [];
+
+        $scope.dataListOptions ={
+            data: 'files',
+            "columnDefs": [
+                {field:'name', displayName:'Name'},
+                {field:'description', displayName:'Description '},
+                {field:'storageType', displayName:'Data Storage Type '}
+            ],
+            enableColumnResize: false,
+            selectedItems: $scope.selectedSet,
+            multiSelect: false
+        };
+
+
     }]);
