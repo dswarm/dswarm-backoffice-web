2dc7e537eacce51842fa6797f5fa09bc2959c5ba
Christof Rodejohann <Christof.Rodejohann@slub-dresden.de>
2013-09-24 13:02:18 +0200

    Code style improvements

--- yo/app/scripts/services/version.js
+++ yo/app/scripts/services/version.js
@@ -1,28 +1,28 @@
 'use strict';
 
 angular.module('dmpApp')
-  .factory('buildInfo', ['$http', function($http) {
-    var promise = $http.get('/data/version.json');
-    promise.get = function(cb) {
-      promise.success(function(resp) {
-        cb(resp.web, resp.api);
-      });
-    };
+    .factory('buildInfo', ['$http', function($http) {
+        var promise = $http.get('/data/version.json');
+        promise.get = function(cb) {
+            promise.success(function(resp) {
+                cb(resp.web, resp.api);
+            });
+        };
 
-    return promise;
-  }])
-  .directive('appVersion', ['buildInfo', function (version) {
-    return function(scope, iElement) {
-      version.get(function(infoWeb, infoApi) {
-        var text = [
-          'Web:', infoWeb.revision,
-          '--',
-          'API:', infoApi.revision,
-          'at',
-          infoWeb.date
-        ];
-        iElement.text(text.join(' '));
-      })
+        return promise;
+    }])
+    .directive('appVersion', ['buildInfo', function (version) {
+        return function(scope, iElement) {
+            version.get(function(infoWeb, infoApi) {
+                var text = [
+                    'Web:', infoWeb.revision,
+                    '--',
+                    'API:', infoApi.revision,
+                    'at',
+                    infoWeb.date
+                ];
+                iElement.text(text.join(' '));
+            });
 
-    };
-  }]);
+        };
+    }]);
