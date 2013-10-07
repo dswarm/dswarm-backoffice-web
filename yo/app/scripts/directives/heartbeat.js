9f78dbf307ba4cbdb39e1a618ce9549593aab864
Paul Horn <knutwalker@gmail.com>
2013-10-07 12:59:23 +0200

    add api status heartbeat indicator
    Signed-off-by: Paul Horn <knutwalker@gmail.com>

---
+++ yo/app/scripts/directives/heartbeat.js
@@ -0,0 +1,56 @@
+'use strict';
+
+angular.module('dmpApp')
+    .directive('heartbeat', ['$timeout', '$http', '$window', function($timeout, $http, $window) {
+        return function heartbeatLinkFn(scope, elem, attrs) {
+            var intervalTime = +scope.$eval(attrs.interval) || 1000,
+                endpoint = scope.$eval(attrs.endpoint),
+                expected = scope.$eval(attrs.expected),
+                api = $window['dmp']['jsRoutes']['api'],
+                config = {
+                    method: 'GET',
+                    url: api + endpoint,
+                    timeout: 50
+                },
+
+                alive = true,
+                cancelHeartbeat,
+
+                interval = function intervalFn() {
+                    cancelHeartbeat = $timeout(function checkHeartbeat() {
+                        $http(config)
+                            .success(function heartbeatSuccess(data) {
+                                elem.removeClass('yellow red green');
+                                elem.addClass((data === expected)? 'green' : 'yellow');
+                            })
+                            .error(function heartbeatSuccess() {
+                                elem.removeClass('yellow green');
+                                elem.addClass('red');
+                            })
+                            .finally(function() {
+                                cancelHeartbeat = $timeout(intervalFn, intervalTime, false);
+                            });
+                    }, intervalTime, false);
+                },
+
+                toggleAlive = function toggleAliveFn(value) {
+                    if (!value && cancelHeartbeat) {
+                        $timeout.cancel(cancelHeartbeat);
+                        cancelHeartbeat = null;
+                        elem.removeClass('yellow red green');
+                        elem.addClass('disabled');
+                    } else if(!cancelHeartbeat) {
+                        elem.removeClass('disabled red green');
+                        elem.addClass('yellow');
+                        interval();
+                    }
+                };
+
+            elem.bind('click', function() {
+                alive = !alive;
+                toggleAlive(alive);
+            });
+
+            toggleAlive(alive);
+        }
+    }]);
