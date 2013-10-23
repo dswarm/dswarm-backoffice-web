289e72ba4e45afdb94a565c60bd58676bfe23dce
Christof Rodejohann <Christof.Rodejohann@slub-dresden.de>
2013-10-23 18:40:59 +0200

    DD-159 usage of additional datasets

--- yo/app/scripts/directives/source-data.js
+++ yo/app/scripts/directives/source-data.js
@@ -1,84 +1,87 @@
 'use strict';
 
 angular.module('dmpApp')
-    .controller('SourceDataCtrl', ['$scope', '$http', '$q', 'schemaParser', 'SchemaDataResource', function ($scope, $http, $q, schemaParser, SchemaDataResource) {
+    .controller('SourceDataCtrl', ['$scope', '$http', '$q', 'schemaParser', 'SchemaDataResource', 'PubSub', function ($scope, $http, $q, schemaParser, SchemaDataResource, PubSub) {
         $scope.internalName = 'Source Data Widget';
 
         $scope.data = {};
 
         $scope.showData = false;
 
+        $scope.resourceName = '';
+
         $scope.dataInclude = function() {
             return $scope.showData ? 'sourcedata' : '';
         };
 
-        var schemaPromise, dataPromise,
-            schemaTransformer, dataTransformer,
-            scopeSetter;
+        PubSub.subscribe($scope, 'handleLoadData', function(args) {
 
-        if ($scope.resourceId && $scope.configId) {
-            schemaPromise = SchemaDataResource.schema({
-                id: $scope.resourceId,
-                cid: $scope.configId
-            }).$promise;
+            if(args && args.resourceId) {
+                $scope.loadData(args.resourceId, args.configId, args.resourceName);
+            } else {
+                $scope.data = {};
+                $scope.showData = false;
+                $scope.resourceName = '';
+            }
 
-            schemaTransformer = function(res) {
-                return res['data'];
-            };
-
-            dataPromise = SchemaDataResource.data({
-                id: $scope.resourceId,
-                cid: $scope.configId,
-                atMost: 3
-            }).$promise;
-
-            dataTransformer = angular.identity;
-
-            scopeSetter = function(schemaResult, dataResult) {
-
-                var records = [];
-                angular.forEach(dataResult, function(record) {
-                    records.push({
-                        id: record.recordId,
-                        data: schemaParser.parseAny(record, schemaResult['title'], schemaResult)
-                    });
-                });
-
-                $scope.records = records;
-
-                $scope.showData = true;
-
-            };
-
-        } else {
-            schemaPromise = $http.get('/data/schema.json');
-            dataPromise = $http.get('/data/record.json');
-
-            schemaTransformer = function(res) {
-                return res['data'];
-            };
-
-            dataTransformer = function(res) {
-                return res['data'];
-            };
-
-            scopeSetter = function(schemaResult, dataResult) {
-                $scope.data = schemaParser.parseAny(
-                    dataResult[schemaResult['title']], schemaResult['title'], schemaResult);
-
-                $scope.showData = true;
-
-            };
-        }
-
-        var allPromise = $q.all([schemaPromise, dataPromise]);
-
-        allPromise.then(function (result) {
-            var schemaResult = schemaTransformer(result[0])
-                , dataResult = dataTransformer(result[1]);
-
-            scopeSetter(schemaResult, dataResult);
         });
+
+        $scope.loadData = function(resourceId, configId, resourceName) {
+
+            var schemaPromise, dataPromise,
+                schemaTransformer, dataTransformer,
+                scopeSetter;
+
+            $scope.resourceName = resourceName;
+
+            if (resourceId && configId) {
+                schemaPromise = SchemaDataResource.schema({
+                    id: $scope.resourceId,
+                    cid: $scope.configId
+                }).$promise;
+
+                schemaTransformer = function(res) {
+                    return res['data'];
+                };
+
+                dataPromise = SchemaDataResource.data({
+                    id: $scope.resourceId,
+                    cid: $scope.configId,
+                    atMost: 3
+                }).$promise;
+
+                dataTransformer = angular.identity;
+
+                scopeSetter = function(schemaResult, dataResult) {
+
+                    var records = [];
+                    angular.forEach(dataResult, function(record) {
+                        records.push({
+                            id: record.recordId,
+                            data: schemaParser.parseAny(record, schemaResult['title'], schemaResult)
+                        });
+                    });
+
+                    $scope.records = records;
+
+                    $scope.showData = true;
+
+                };
+
+            }
+
+            var allPromise = $q.all([schemaPromise, dataPromise]);
+
+            allPromise.then(function (result) {
+                var schemaResult = schemaTransformer(result[0])
+                    , dataResult = dataTransformer(result[1]);
+
+                scopeSetter(schemaResult, dataResult);
+            });
+
+        };
+
+
     }])
     .directive('sourceData', [ function () {
         return {
