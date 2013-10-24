bb822544bbe1c65f9827dd7f5b0bb1b10c73653a
Christof Rodejohann <Christof.Rodejohann@slub-dresden.de>
2013-10-24 09:12:08 +0200

    Remove tests for static source and schema loading

--- yo/test/spec/controllers/source-data.js
+++ yo/test/spec/controllers/source-data.js
@@ -46,31 +46,7 @@ describe('Controller: SourceDataCtrl', function () {
 
     it('should have a SchemaCtrl controller', function() {
         var SourceDataCtrl = sourceDataCtrl();
-        $rootScope.$digest();
-        $httpBackend.flush();
         expect(SourceDataCtrl).not.toBe(null);
-    });
-
-    it('should have loaded schema data', function () {
-        $httpBackend.expectGET('/data/schema.json');
-        sourceDataCtrl();
-        $rootScope.$digest();
-        $httpBackend.flush();
-
-        expect(scope.data.name).toBe('OAI-PMH');
-
-        expect(scope.data.children.length).toBe(3);
-
-    });
-
-    it('should have loaded record data', function () {
-        $httpBackend.expectGET('/data/record.json');
-        sourceDataCtrl();
-        $rootScope.$digest();
-        $httpBackend.flush();
-
-        expect(scope.data.children[0].children[0].children[0].children[0].title).toBe('urn:nbn:de:bsz:14-ds-1229427875176-76287');
-
     });
 
 });
