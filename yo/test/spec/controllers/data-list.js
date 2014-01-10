ebf3f6ad48a3467431d98319ddd97286a9c83885
chrode <christof.rodejohann@slub-dresden.de>
2014-01-10 17:49:36 +0100

    DD-275 Add tests

--- yo/test/spec/controllers/data-list.js
+++ yo/test/spec/controllers/data-list.js
@@ -75,6 +75,48 @@ describe('Controller: DataListCtrl', function () {
     angular.forEach(models, function(data, idx) {
       compare(data, scope.models[idx]);
     });
-  })
+  });
+
+  it('should generate a correct new project call', inject(function(ProjectResource) {
+
+      var ctrl, flag, value;
+      $httpBackend.expectGET('/dmp/resources');
+      $httpBackend.expectGET('/dmp/projects');
+      scope.$apply(function() {
+          ctrl = dataListCtrl();
+      });
+      $httpBackend.flush();
+
+      spyOn(ProjectResource, "save");
+
+      scope.onUseForNewProjectClick([{ id : 2}], { name : 'Foo', description : 'Bar'});
+
+      var callArgs = ProjectResource.save.mostRecentCall.args;
+
+      expect(callArgs[1].input_data_model.id).toBe(2);
+      expect(callArgs[1].name).toBe('Foo');
+      expect(callArgs[1].description).toBe('Bar');
+
+  }));
+
+  it('data should be loaded', function() {
+
+      var ctrl;
+
+      $httpBackend.expectGET('/dmp/resources');
+      $httpBackend.expectGET('/dmp/projects');
+
+      scope.$apply(function() {
+          ctrl = dataListCtrl();
+      });
+      $httpBackend.flush();
+
+      $httpBackend.expectGET('/dmp/projects');
+
+      scope.updateGridData();
+
+      expect(scope.projects[0].id).toBe(3);
+
+  });
 
 });
