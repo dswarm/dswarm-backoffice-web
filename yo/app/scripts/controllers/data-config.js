90373643004a96d49b0a264a3bb9dfe2ce8f9d74
Christof Rodejohann <christof@rodejohann.de>
2015-02-20 18:02:25 +0100

    [BUGFIX] [DD-915] Fix regression causing unloadable config view.

--- yo/app/scripts/controllers/data-config.js
+++ yo/app/scripts/controllers/data-config.js
@@ -18,6 +18,8 @@
 angular.module('dmpApp')
     .controller('DataConfigCtrl', function($scope, ngProgress, $routeParams, $location) {
 
+        $scope.configType = $routeParams.configType;
+
         $scope.mode = $routeParams.dataModelId ? 'edit' : 'create';
 
         $scope.configError = '';
