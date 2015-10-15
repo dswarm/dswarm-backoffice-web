9cbab69d71726da32617738ceb893ac8edb7dc30
Thomas Gängler <tgaengler@avantgarde-labs.de>
2015-10-15 17:38:04 +0200

    [DD-1232] set up copy-or-migrate-mappings widget skeleton

--- yo/app/scripts/controllers/data-list.js
+++ yo/app/scripts/controllers/data-list.js
@@ -1,12 +1,12 @@
 /**
  * Copyright (C) 2013 – 2015  SLUB Dresden & Avantgarde Labs GmbH (<code@dswarm.org>)
- *  
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *  
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- *  
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -180,5 +180,23 @@ angular.module('dmpApp')
 
         $scope.updateGridData();
 
+        function openCopyOrMigrateMappings() {
 
+            var modalInstance = $modal.open({
+                templateUrl: 'views/directives/copy-or-migrate-mappings.html',
+                controller: 'CopyOrMigrateMappingsCtrl',
+                windowClass: 'wide',
+                resolve: {}
+            });
+
+            modalInstance.result.then(function(reason) {
+
+                // TODO
+            });
+        }
+
+        $scope.onCopyOrMigrateMappingsClick = function() {
+
+            openCopyOrMigrateMappings();
+        };
     });
